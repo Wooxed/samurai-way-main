eae7cbe6d73eaa71880d3ef51c7d1968c34b41f7
Sergei <samkorbut@gmail.com>
2022-10-12 07:35:44 +0400

    Add Header.tsx, Navbar.tsx, Profile.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Header = () => {
+    return <header className={"header"}>
+        <img src={'https://www.logomoose.com/wp-content/uploads/2015/07/Studio-St-Louis_monogram.png'}/>
+    </header>
+}

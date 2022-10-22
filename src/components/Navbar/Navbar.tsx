3642cb6c4d91dff0f9a113bccd0f32a579eefb91
Sergei <samkorbut@gmail.com>
2022-10-22 10:43:35 +0400

    Refactor Navbar, add Navlink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import classes from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 
 export const Navbar = () => {
-    return <nav className={classes.nav}>
-        <div className={classes.item}>
-            <a>Profile</a>
-        </div>
-        <div className={classes.item}>
-            <a>Messages</a>
-        </div>
-        <div>
-            <a className={classes.item}>News</a>
-        </div>
-        <div>
-            <a className={classes.item}>Music</a>
-        </div>
-        <div>
-            <a className={classes.item}>Settings</a>
-        </div>
-    </nav>
+    return (
+        <nav className={classes.nav}>
+            <div className={classes.item}>
+                <NavLink to={'./profile'} activeClassName={classes.active}>Profile</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to={'/dialogs'} activeClassName={classes.active}>Messages</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to={'/news'} activeClassName={classes.active}>News</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to={'/music'} activeClassName={classes.active}>Music</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to={'/settings'} activeClassName={classes.active}>Settings</NavLink>
+            </div>
+        </nav>
+    )
 }
 

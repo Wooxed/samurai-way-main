6ab92cbcad0cdc6e60a5c3448682c75592007bdf
Sergei <samkorbut@gmail.com>
2022-10-23 10:10:33 +0400

    Refactor component <Profile/> and add css for Profile.module.css

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import classes from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./MyPosts/ProfileInfo/ProfileInfo";
 
 export const Profile = () => {
     return (
-        <div className={classes.content}>
-            <div>
-                <img src={"https://kogdakotika.net/media/post_images/title_page_m_ChtRYfI.jpg"} alt={'image'}/>
-            </div>
-            <div>ava + description</div>
+        <div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
 
